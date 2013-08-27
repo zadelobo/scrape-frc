92e9d5391a87d6f08109f319b52d310cba6dc441
Zach Orr <zachorr139@gmail.com>
2013-08-27 16:13:32 -0400

    Code to get WLT from TBA - needs work

--- scrape-id-numbers.go
+++ scrape-id-numbers.go
@@ -12,6 +12,10 @@ type Team struct {
   state, city, teamName, teamID, teamNumber string
 }
 
+type WLT struct {
+  teamNumber, w, l, t string
+}
+
 func getNumberOfPages() (num int, err error) {
   resp, err := http.Get("http://www.usfirst.org/whats-going-on/teams?page=0&ProgramCode=FRC&Season=2013&Country=USA&sort=asc&order=Team%%20Number")
   if err != nil {
@@ -45,27 +49,51 @@ func getTeams(url string, c chan<- []Team) {
   c <-teams
 }
 
+func getWLT(teamNumber string, c chan<- WLT) {
+  url := fmt.Sprintf("http://www.thebluealliance.com/team/%s/2013", teamNumber)
+  resp, err := http.Get(url)
+  if err != nil {
+    return
+  }
+  defer resp.Body.Close()
+  contents, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    // Handle error
+  }
+  re, _ := regexp.Compile(`<strong>([\d]{1,2})-([\d]{1,2})-([\d]{1,2})</strong>`)
+  res := re.FindAllStringSubmatch(string(contents), -1)
+  if len(res) > 0 {
+    wlt := WLT{teamNumber, res[0][1], res[0][2], res[0][3]}
+    c <-wlt
+  } else {
+    c <-WLT{teamNumber, "", "", ""}
+  }
+}
+
 func main() {
   // Do a call to get a list of all of the teams (2013)
-  c := make(chan []Team)
+  c1 := make(chan []Team)
+  c2 := make(chan WLT)
   n := 0
   // Check how many teams to get
   numPages, err := getNumberOfPages()
   if err != nil {
     return
   }
-  fmt.Println(numPages)
   for i := 0; i <= numPages; i++ {
     url := fmt.Sprintf("http://www.usfirst.org/whats-going-on/teams?page=%d&ProgramCode=FRC&Season=2013&Country=USA&sort=asc&order=Team%%20Number", i)
-    go getTeams(url, c)
+    go getTeams(url, c1)
     n++
   }
-  urlArray := make(map[string] string)
+  // urlArray := make(map[string] string)
   for i := n; i > 0; i-- {
-    n := <-c
-    for _, team := range n {
-      append(urlArray, "")
-      fmt.Println(team)
+    tt := <-c1
+    for _, team := range tt {
+      go getWLT(team.teamNumber, c2)
     }
+  }
+  fmt.Println(n)
+  for i := n; i > 0; i-- {
+    fmt.Println(<-c2)
   }
 }
