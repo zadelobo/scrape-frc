04619fd0dfc6594a951003f4b8632fe7e803cf17
Zach Orr <zachorr139@gmail.com>
2013-09-01 23:04:06 -0400

    Gets all the teams now in all the countries

--- scrape-id-numbers.go
+++ scrape-id-numbers.go
@@ -6,6 +6,7 @@ import (
   "io/ioutil"
   "regexp"
   "strconv"
+  "strings"
 )
 
 type Team struct {
@@ -16,26 +17,36 @@ type WLT struct {
   teamNumber, w, l, t string
 }
 
-func getNumberOfPages() (num int, err error) {
-  resp, err := http.Get("http://www.usfirst.org/whats-going-on/teams?page=0&ProgramCode=FRC&Season=2013&Country=USA&sort=asc&order=Team%%20Number")
+func getPageContent(url string) (response string, err error) {
+  resp, err := http.Get(url)
   if err != nil {
-    return -1, err
+    return "", err
   }
   defer resp.Body.Close()
   contents, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    return "", err
+  }
+  return string(contents), nil
+}
+
+func getNumberOfPages(country string) (num int, err error) {
+  url := fmt.Sprintf("http://www.usfirst.org/whats-going-on/teams?page=0&ProgramCode=FRC&Season=2013&Country=%s&sort=asc&order=Team%%20Number", country)
+  contents, err := getPageContent(url)
+  if err != nil {
+    return 0, err
+  }
   re, _ := regexp.Compile(`<a title="Go to last page" href="/whats-going-on/teams\?page=([\d]+?)&amp`)
-  res := re.FindStringSubmatch(string(contents))
-  num, _ = strconv.Atoi(res[1])
+  res := re.FindStringSubmatch(contents)
+  num = 0
+  if len(res) > 0 {
+    num, _ = strconv.Atoi(res[1])
+  }
   return num, nil
 }
 
 func getTeams(url string, c chan<- []Team) {
-  resp, err := http.Get(url)
-  if err != nil {
-    return
-  }
-  defer resp.Body.Close()
-  contents, err := ioutil.ReadAll(resp.Body)
+  contents, err := getPageContent(url)
   if err != nil {
     // Handle error
   }
@@ -51,17 +62,13 @@ func getTeams(url string, c chan<- []Team) {
 
 func getWLT(teamNumber string, c chan<- WLT) {
   url := fmt.Sprintf("http://www.thebluealliance.com/team/%s/2013", teamNumber)
-  resp, err := http.Get(url)
-  if err != nil {
-    return
-  }
-  defer resp.Body.Close()
-  contents, err := ioutil.ReadAll(resp.Body)
+  contents, err := getPageContent(url)
   if err != nil {
     // Handle error
   }
   re, _ := regexp.Compile(`<strong>([\d]{1,2})-([\d]{1,2})-([\d]{1,2})</strong>`)
   res := re.FindAllStringSubmatch(string(contents), -1)
+  fmt.Println(res)
   if len(res) > 0 {
     wlt := WLT{teamNumber, res[0][1], res[0][2], res[0][3]}
     c <-wlt
@@ -70,21 +77,45 @@ func getWLT(teamNumber string, c chan<- WLT) {
   }
 }
 
+func getCountries() (countryArray []string, err error) {
+  contents, err := getPageContent("http://www.usfirst.org/whats-going-on")
+  if err != nil {
+    return
+  }
+  reOuter, _ := regexp.Compile(`<select id="edit-country--2"(.*)`)
+  reInner, _ := regexp.Compile(`<option value="(.*?)"`)
+  resOuter := reOuter.FindString(contents)
+  resInner := reInner.FindAllStringSubmatch(resOuter, -1)
+  countryArray = make([]string, 0)
+  for _, country := range resInner {
+    c := strings.Replace(country[1], " ", "+", -1)
+    countryArray = append(countryArray, c)
+  }
+  return countryArray, nil
+}
+
 func main() {
   // Do a call to get a list of all of the teams (2013)
   c1 := make(chan []Team)
   c2 := make(chan WLT)
   n1 := 0
   // Check how many teams to get
-  numPages, err := getNumberOfPages()
+  countries, err := getCountries()
   if err != nil {
     return
   }
-  fmt.Println(numPages)
-  for i := 0; i <= numPages; i++ {
-    url := fmt.Sprintf("http://www.usfirst.org/whats-going-on/teams?page=%d&ProgramCode=FRC&Season=2013&Country=USA&sort=asc&order=Team%%20Number", i)
-    go getTeams(url, c1)
-    n1++
+  for _, country := range countries {
+    fmt.Println(country)
+    numPages, err := getNumberOfPages(country)
+    if err != nil {
+      continue
+    }
+    fmt.Println(numPages)
+    for i := 0; i <= numPages; i++ {
+      url := fmt.Sprintf("http://www.usfirst.org/whats-going-on/teams?page=%d&ProgramCode=FRC&Season=2013&Country=%s&sort=asc&order=Team%%20Number", i, country)
+      go getTeams(url, c1)
+      n1++
+    }
   }
   n2 := 0
   // urlArray := make(map[string] string)
@@ -96,6 +127,7 @@ func main() {
     }
     fmt.Println(i)
   }
+  fmt.Println(n2)
   for i := n2; i > 0; i-- {
     fmt.Println(<-c2)
   }
